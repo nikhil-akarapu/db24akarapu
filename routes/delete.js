72e02d018fdbddd4f8f8fbeb221e0336a1e04589
nikhil-akarapu <37258852+nikhil-akarapu@users.noreply.github.com>
2021-11-18 10:02:07 -0600

    modified files

--- routes/delete.js
+++ routes/delete.js
@@ -2,7 +2,7 @@ var express = require('express');
 const magpie_controlers= require('../controllers/magpie');
 var router = express.Router();
 
-/* GET create costume page */ 
-router.get('/delete', costume_controlers.costume_delete_Page)
+/* GET create magpie page */ 
+router.get('/delete', magpie_controlers.magpie_delete_Page)
 
 module.exports = router;
